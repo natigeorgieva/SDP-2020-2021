010cbc20cb692ee478f39296d9cfa95be1c5f5f5
Stefcho27 <72232154+Stefcho27@users.noreply.github.com>
2021-03-14 18:11:02 +0200

    Update Task 3.cpp

--- Week 03/Task 3.cpp
+++ Week 03/Task 3.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-bool AreTheFirstNTheSame(char* ptr1, char*ptr2,int n)
+bool AreTheFirstNTheSame(char* ptr1, char*ptr2, int n)
 {
 	if (n > Strlen(ptr1)+1 || n > Strlen(ptr2)+1)// Функция от 1-та задача
 		return 0;
@@ -9,8 +9,13 @@ bool AreTheFirstNTheSame(char* ptr1, char*ptr2,int n)
 	//Вече сме си гарантирали, че и двата масива имат дължина поне n (това включва и терминиращия символ)
   
 	for (int i = 0; i < n; i++)
-		if (ptr1[i] != ptr2[i])
+	{
+		if (isalpha(ptr1[i]) && isalpha(ptr2[i]) && (ptr1[i] == ptr2[i] + 'A' - 'a' || ptr1[i] == ptr2[i] + 'a' - 'A'))
+			continue;
+		else if (ptr1[i] != ptr2[i])
 			return 0;
+			
+	}
 
 	return 1;
 }
