d567e1f88398e3cbe4aab7ca72f2402c501f6f98
Stefcho27 <72232154+Stefcho27@users.noreply.github.com>
2021-04-22 12:56:17 +0300

    Update task5.cpp

--- Stack/task5.cpp
+++ Stack/task5.cpp
@@ -2,10 +2,11 @@ void sort(stack<int>& original)
 {
 	stack<int>temp;
 	int len = original.size();
-	for (int i = 0; i < len; i++)
+	for (int i = 0; i < len; i++)//С външния цикъл сортираме всяко от числата
 	{
 		int largest = original.top();
-		for (int j = 0; j < len - i; j++)
+		for (int j = 0; j < len - i; j++)//С този цикъл прехвърляме всички len-i на брой числа в друг масив, като в процеса прверяваме кое е най-голямото от тях
+		//Броят е len-i, тъй като на всяка стъпка от външния цикъл ние сме сортирали вече i елемента и се занимаваме с оставащите
 		{
 			if (largest < original.top())
 				largest = original.top();
@@ -14,7 +15,8 @@ void sort(stack<int>& original)
 		}
 		original.push(largest);
 		bool flag=true;
-		for (int j = 0; j < len - i; j++)
+		for (int j = 0; j < len - i; j++)//Тук връщаме числата от втория масив обратно в първия, като при първото срещане на елемента largest, гo пропускаме,
+			//тъй като вече сме го записали в първия масив на правилното му място.
 		{
 			if (flag && temp.top() == largest)
 			{
